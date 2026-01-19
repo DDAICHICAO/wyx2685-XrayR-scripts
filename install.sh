169666e6e2758bd94e2f0bde6833a30bae66693f
toamsterron <toamsterron@noreply.gitcode.com>
2026-01-19 20:03:43 +0800

    Fix: download binaries from original repository

--- install.sh
+++ install.sh
@@ -110,7 +110,7 @@ install_XrayR() {
 
     if  [ $# == 0 ] ;then
         # 获取最新的 release
-        last_version=$(curl -Ls "https://api.github.com/repos/DDAICHICAO/wyx2685-XrayR/releases" | grep '"tag_name":' | head -1 | sed -E 's/.*"([^"]+)".*/\1/')
+        last_version=$(curl -Ls "https://api.github.com/repos/leaderen/wyx2685-XrayR/releases" | grep '"tag_name":' | head -1 | sed -E 's/.*"([^"]+)".*/\1/')
         if [[ ! -n "$last_version" ]]; then
             echo -e "${red}检测 XrayR 版本失败，可能是超出 Github API 限制，请稍后再试，或手动指定 XrayR 版本安装${plain}"
             echo -e "${yellow}提示：可以手动指定版本安装，例如：bash install.sh v0.9.2${plain}"
@@ -119,12 +119,12 @@ install_XrayR() {
         echo -e "检测到 XrayR 最新版本：${last_version}，开始安装"
         
         # 尝试下载最新版本
-        wget -q -N --no-check-certificate -O /usr/local/XrayR/XrayR-linux.zip https://github.com/DDAICHICAO/wyx2685-XrayR/releases/download/${last_version}/XrayR-linux-${arch}.zip
+        wget -q -N --no-check-certificate -O /usr/local/XrayR/XrayR-linux.zip https://github.com/leaderen/wyx2685-XrayR/releases/download/${last_version}/XrayR-linux-${arch}.zip
         if [[ $? -ne 0 ]]; then
             echo -e "${yellow}${last_version} 下载失败，尝试 v0.9.2...${plain}"
             # 如果最新版本下载失败，尝试 v0.9.2
             last_version="v0.9.2"
-            wget -q -N --no-check-certificate -O /usr/local/XrayR/XrayR-linux.zip https://github.com/DDAICHICAO/wyx2685-XrayR/releases/download/${last_version}/XrayR-linux-${arch}.zip
+            wget -q -N --no-check-certificate -O /usr/local/XrayR/XrayR-linux.zip https://github.com/leaderen/wyx2685-XrayR/releases/download/${last_version}/XrayR-linux-${arch}.zip
             if [[ $? -ne 0 ]]; then
                 echo -e "${red}下载 XrayR 失败，请确保你的服务器能够下载 Github 的文件${plain}"
                 exit 1
@@ -136,7 +136,7 @@ install_XrayR() {
 	else
 	    last_version="v"$1
 	fi
-        url="https://github.com/DDAICHICAO/wyx2685-XrayR/releases/download/${last_version}/XrayR-linux-${arch}.zip"
+        url="https://github.com/leaderen/wyx2685-XrayR/releases/download/${last_version}/XrayR-linux-${arch}.zip"
         echo -e "开始安装 XrayR ${last_version}"
         wget -q -N --no-check-certificate -O /usr/local/XrayR/XrayR-linux.zip ${url}
         if [[ $? -ne 0 ]]; then
